ba46369c75ea121ecf6c182691a96078fec682ea
artisgarbage <artisgarbage@gmail.com>
2017-06-07 21:20:52 -0600

    WIP

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap'
 
 
 // Exports
