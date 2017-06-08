7fed02df2120c4b3a77d9b438dc76d341dcca88c
artisgarbage <artisgarbage@gmail.com>
2017-06-07 18:02:09 -0600

    production Brunch overrides

--- brunch-config.js
+++ brunch-config.js
@@ -9,11 +9,18 @@ exports.files = {
       'app.css': /^app/
     }
   }
-};
+}
 
 exports.plugins = {
   babel: {presets: ['latest', 'react']},
   stylus: { modules: true }
-};
+}
+
+exports.overrides = {
+  production: {
+    optimize: true,
+    sourceMaps: false
+  }
+}
 
 exports.hot = true;
